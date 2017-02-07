f750b9a19b0e4bd4314453474077d89fe89ff32d
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-02-07 19:06:27 +0000

    Added demo of thenCompose for basis of writeup

--- src/test/java/com/millross/blog/async/SimpleCompletableFutureTest.java
+++ src/test/java/com/millross/blog/async/SimpleCompletableFutureTest.java
@@ -14,23 +14,51 @@ public class SimpleCompletableFutureTest {
 
     @Test
     public void testSimpleFutureCompletion() throws Exception {
-        final CompletableFuture<Integer> future = blockingSuccessFuture();
+        final CompletableFuture<Integer> future = blockingSuccessFuture(1);
         assertThat(future.get(), is(1));
     }
 
     @Test
     public void demoThenApply() throws Exception {
-        final CompletableFuture<Integer> future = blockingSuccessFuture()
+        final CompletableFuture<Integer> future = blockingSuccessFuture(1)
                 .thenApply(i -> i + 3);
         assertThat(future.get(), is(4));
     }
 
-    public CompletableFuture<Integer> blockingSuccessFuture() {
+    @Test
+    public void demoThenCompose() throws Exception {
+        // We'll use one future to specify how long a subsequent future will take
+        final CompletableFuture<Integer> future = blockingSuccessFuture(3)
+                .thenApply(i -> {
+                    System.out.println("First future completed, " + i);
+                    return i;
+                })
+                .thenCompose(i ->  specifiedDelayFuture(i * 100, 2));
+        assertThat(future.get(), is(2));
+    }
+
+    public CompletableFuture<Integer> blockingSuccessFuture(final int value) {
         final CompletableFuture<Integer> future = new CompletableFuture<Integer>();
         final Thread futureThread = new Thread(() -> {
             try {
                 Thread.sleep(1000);
-                future.complete(1);
+                future.complete(value);
+            } catch (Throwable t) {
+                future.completeExceptionally(t);
+            }
+
+        });
+        // Kick off processing
+        futureThread.run();
+        return future;
+    }
+
+    public CompletableFuture<Integer> specifiedDelayFuture(final int delay, final int finalValue) {
+        final CompletableFuture<Integer> future = new CompletableFuture<>();
+        final Thread futureThread = new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                future.complete(finalValue);
             } catch (Throwable t) {
                 future.completeExceptionally(t);
             }
