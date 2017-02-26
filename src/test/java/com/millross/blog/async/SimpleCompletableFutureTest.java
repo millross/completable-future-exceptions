506f27578ca38f3bb5966e5441ebe02492369807
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-02-26 15:46:22 +0000

    Replace blockingSuccessFuture with use of an existing CompletableFuture factory method. Let's not roll our own futures where we don't need to!

--- src/test/java/com/millross/blog/async/SimpleCompletableFutureTest.java
+++ src/test/java/com/millross/blog/async/SimpleCompletableFutureTest.java
@@ -3,7 +3,11 @@ package com.millross.blog.async;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
 
+import static java.util.concurrent.CompletableFuture.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,15 +16,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class SimpleCompletableFutureTest {
 
+    // We'll allow a 3-sized thread pool for async execution, rather than using the fork/join pool for asynchronous
+    // value generation
+    final Executor executor = Executors.newFixedThreadPool(3);
+
     @Test
     public void testSimpleFutureCompletion() throws Exception {
-        final CompletableFuture<Integer> future = blockingSuccessFuture(1);
+        final CompletableFuture<Integer> future = supplyAsync(delayedValueSupplier(1), executor);
         assertThat(future.get(), is(1));
     }
 
     @Test
     public void demoThenApply() throws Exception {
-        final CompletableFuture<Integer> future = blockingSuccessFuture(1)
+        final CompletableFuture<Integer> future = supplyAsync(delayedValueSupplier(1), executor)
                 .thenApply(i -> i + 3);
         assertThat(future.get(), is(4));
     }
@@ -28,29 +36,24 @@ public class SimpleCompletableFutureTest {
     @Test
     public void demoThenCompose() throws Exception {
         // We'll use one future to specify how long a subsequent future will take
-        final CompletableFuture<Integer> future = blockingSuccessFuture(3)
+        final CompletableFuture<Integer> future = supplyAsync(delayedValueSupplier(3), executor)
                 .thenApply(i -> {
                     System.out.println("First future completed, " + i);
                     return i;
                 })
-                .thenCompose(i ->  specifiedDelayFuture(i * 100, 2));
+                .thenCompose(i -> specifiedDelayFuture(i * 100, 2));
         assertThat(future.get(), is(2));
     }
 
-    public CompletableFuture<Integer> blockingSuccessFuture(final int value) {
-        final CompletableFuture<Integer> future = new CompletableFuture<Integer>();
-        final Thread futureThread = new Thread(() -> {
+    public Supplier<Integer> delayedValueSupplier(final int value) {
+        return () -> {
             try {
                 Thread.sleep(1000);
-                future.complete(value);
-            } catch (Throwable t) {
-                future.completeExceptionally(t);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Problem while waiting to return value");
             }
-
-        });
-        // Kick off processing
-        futureThread.run();
-        return future;
+            return value;
+        };
     }
 
     public CompletableFuture<Integer> specifiedDelayFuture(final int delay, final int finalValue) {
